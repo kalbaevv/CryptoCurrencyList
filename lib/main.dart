ffca30fccf329e124597ced1b36bfae6e1dc3050
iskender <even10iq@gmail.com>
2023-10-16 20:37:39 +0000

    added crypto_coins_list_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:crypto_coins_list/router/router.dart';
-import 'package:crypto_coins_list/theme/theme.dart';
+import 'package:crypto_coins_list/crypto_coins_list_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const CryptoCurrenciesListApp());
-}
-
-class CryptoCurrenciesListApp extends StatelessWidget {
-  const CryptoCurrenciesListApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: ' Crypto CurrenciesL ist',
-      theme: darkTheme,
-      routes: routes,
-    );
-  }
 }
